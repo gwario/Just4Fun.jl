8f15185b1d9637458d5befe36609261172f94383
Mario Gastegger <mario.gastegger@gmail.com>
2022-09-04 18:02:15 +0200

    Add export for expore mode

--- src/Just4Fun.jl
+++ src/Just4Fun.jl
@@ -10,8 +10,8 @@ include("game/main.jl")
 
 export Just4FunEnv, Just4FunSpec, CardValue, Cards, Stones, FieldValue, CardsAction
 export Player, YELLOW, RED, GREEN, MAGENTA
-export GameState
-export in_progress, end_by_pattern, end_by_points, end_by_max_field, end_by_draw
+export GameState, in_progress, end_by_pattern, end_by_points, end_by_max_field, end_by_draw
+export CARD_ACTION_SEPARATOR
 # TODO: make configurable in spec maybe
 export FIELD_VALUES
 export FEATURE_CARDS
