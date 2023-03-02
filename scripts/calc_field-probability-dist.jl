a69a6aea85c0f06c73a3817cdfc95aacf715ebc4
Mario Gastegger <mario.gastegger@gmail.com>
2023-03-02 16:15:02 +0100

    Add config for mini just 4 fun

--- scripts/calc_field-probability-dist.jl
+++ scripts/calc_field-probability-dist.jl
@@ -3,15 +3,15 @@ using Printf
 
 include("statistics.jl")
 
-const N_SAMPLES = 1_000_000_000
+const N_SAMPLES = 1_000_000 #1_000_000_000
 
 stats = Dict{FieldValue, Int64}()
 
 
 for _ in 1:N_SAMPLES
-    hand = get_random_hand(Just4Fun.JUST4FUN_DEFAULT_SIZE_HAND, Just4Fun.JUST4FUN_DEFAULT_DECK)
+    hand = get_random_hand(Just4Fun.SIZE_HAND, map(Int64, Just4Fun.DECK))
 
-    fields = reachable_fields(vec(Just4Fun.JUST4FUN_DEFAULT_ORIGINAL_DIST), hand)
+    fields = reachable_fields(vec(Just4Fun.FIELD_VALUES_DIST), hand)
     
     for field in fields
         stats[field] = get(stats, field, 0) + 1
@@ -30,9 +30,9 @@ for (key, value) in sort(collect(stats), by=pair -> pair[1])
     @printf("%+2s: %4.2f\n", key, value / N_SAMPLES)
 end
 
-mapped = zeros(size(Just4Fun.JUST4FUN_DEFAULT_ORIGINAL_DIST))
+mapped = zeros(size(Just4Fun.FIELD_VALUES_DIST))
 
-for (i,v) in enumerate(Just4Fun.JUST4FUN_DEFAULT_ORIGINAL_DIST)
+for (i,v) in enumerate(Just4Fun.FIELD_VALUES_DIST)
     mapped[i] = stats[v]
 end
 
